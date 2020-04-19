d5632e70a0b74dd16c8ab1b5463a8f1249dc64ae
LonelyCats <priorov.03@mail.ru>
2020-04-19 05:19:15 +0300

    Add .idea

--- ShagBot.py
+++ ShagBot.py
@@ -2,109 +2,223 @@ import telebot
 import pyqrcode
 from telebot import types
 
-bot = telebot.TeleBot('947044314:AAEsRIqbGcz5NtLJcyPJkjjVN6XD0ukKT_0')
+bot = telebot.TeleBot('1107170969:AAEcKoxUynG08lbnHFajPWu9gcw_pXZMUg8')
 
-keyboard1 = telebot.types.ReplyKeyboardMarkup()
-keyboard1.row('Мне нужна помощь!', 'QR (test)')
+# ----------------------------------------------------------Кнопки языка при старте----------------------------------------------------------
+btnLanguage = telebot.types.ReplyKeyboardMarkup()
+btnLanguage.row('Русский')
+btnLanguage.row('English')
 
-keyboard2 = telebot.types.ReplyKeyboardMarkup()
-keyboard2.row('Где и когда пара?')
-keyboard2.row('Когда каникулы?')
-keyboard2.row('<Назад>')
-
-keyboard21 = telebot.types.ReplyKeyboardMarkup()
-keyboard21.row('Средний балл?', 'Посещаемость?', 'Мероприятия?')
-keyboard21.row('Стоимость обучения?', 'Когда конец обучения?','Отзывы преподователей?')
-keyboard21.row('<Назад>')
-
-keyboard3 = telebot.types.ReplyKeyboardMarkup()
-keyboard3.row('1.Родитель')
-keyboard3.row('2.Ученик')
-keyboard3.row('<Возврат в начало>')
-
-keyboard = telebot.types.InlineKeyboardMarkup()
-key_telec = telebot.types.InlineKeyboardButton('летний лагерь', callback_data='meropr')
-keyboard.add(key_telec)
-
-pay = str(11999)
-sredball = str(11)
-poseham = str(80)
-otziv = str('ведет себя хорошо')
-otzivyh = str('Леонид')
-klass = '6.2'
-time = '12:00'
-
-a = str('1 июня')  # Просто переменная, чтобы не ругался компилятор
-b = str('31 августа')  # Просто переменная, чтобы не ругался компилятор
-
-#-----------------------------------------------------------------------------------------------------------------------
-
-wwwqr = "https://www.youtube.com/watch?v=c8bOFSmVSuQ"
-url = pyqrcode.create(wwwqr)
-url.svg("myqr.svg", scale = 8)
-
-#-----------------------------------------------------------------------------------------------------------------------
-
-@bot.message_handler(commands=['start','старт'])
+# ----------------------------------------------------------Старт----------------------------------------------------------
+@bot.message_handler(commands=['start', 'старт'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Готов к работе!', reply_markup=keyboard1)
+    bot.send_message(message.chat.id, 'Выберите язык / Select language', reply_markup=btnLanguage)
 
-@bot.message_handler(content_types=['text','Photo'])
-def send_text(message):
-    if message.text.lower() == 'мне нужна помощь!':
-        bot.send_message(message.chat.id, 'Кем вы являетесь?', reply_markup=keyboard3)
+# ----------------------------------------------------------Возврат в начало----------------------------------------------------------
+@bot.message_handler(func=lambda message: message.text.lower() == '<Возврат в начало>'.lower() or message.text.lower() == '<Return to start>'.lower())
+def btn_login_ru(message):
+    start_message(message)
 
-    elif message.text.lower() == 'QR (test)':
-        bot.send_message(message.chat.id, 'qr:')
-
-    elif message.text.lower() == '2.ученик':
-        bot.send_message(message.chat.id, 'Вопросы, которые вы можете задать:', reply_markup=keyboard2)
-
-    elif message.text.lower() == '1.родитель':
-        bot.send_message(message.chat.id, 'Вопросы, которые вы можете задать:', reply_markup=keyboard21)
+# ----------------------------------------------------------Возврат назад----------------------------------------------------------
+@bot.message_handler(func=lambda message: message.text.lower() == '<назад>'.lower() or message.text.lower() == '<back>'.lower())
+def btn_back(message):
+    btn_welcome(message)
 
 
-    elif message.text.lower() == 'где и когда пара?':
-        bot.send_message(message.chat.id, 'пара в классе: ' + klass + ' в ' + time)
+# ----------------------------------------------------------Справочник языка----------------------------------------------------------
+dictMsg = {
+    'en': {
+        'Забыл логин': 'Forget login',
+        'Забыл пароль': 'Forget password',
+        'Введите пароль': 'Enter password',
+        'Welcome': 'Welcome',
+        '<Возврат в начало>': '<Return to start>',
+        'Ученик': 'Student',
+        'Родитель': 'Parent',
+        'Средний балл': 'Average score',
+        'Посещаемость': 'Attendance',
+        'Мероприятия': 'Events',
+        'Стоимость обучения': 'Cost training',
+        'Конец обучения': 'The end of the training',
+        'Отзывы преподавателей': 'Teachers feedback',
+        'Когда пара?': 'When pairs?',
+        'Где пара?': 'Where pairs?',
+        '<Назад>': '<Back>'
+    },
+    'ru': {
+        'Забыл логин': 'Забыл логин',
+        'Забыл пароль': 'Забыл пароль',
+        'Введите пароль': 'Введите пароль',
+        'Welcome': 'Добро пожаловать!',
+        '<Возврат в начало>': '<Возврат в начало>',
+        'Ученик': 'Ученик',
+        'Родитель': 'Родитель',
+        'Средний балл': 'Средний балл',
+        'Посещаемость': 'Посещаемость',
+        'Мероприятия': 'Мероприятия',
+        'Стоимость обучения': 'Стоимость обучения',
+        'Конец обучения': 'Конец обучения',
+        'Отзывы преподавателей': 'Отзывы преподавателей',
+        'Когда пара?': 'Когда пара?',
+        'Где пара?': 'Где пара?',
+        '<Назад>': '<Назад>'
+    }
+}
 
 
-    elif message.text.lower() == 'посещаемость?':
-        bot.send_message(message.chat.id, 'посещаемость за месяц = ' + poseham + ' %')
-
-    elif message.text.lower() == 'мероприятия?':
-        bot.send_message(message.chat.id, 'Мероприятия: ', reply_markup=keyboard)
-
-    elif message.text.lower() == 'средний балл?':
-        bot.send_message(message.chat.id, 'Средний балл ученик = ' + sredball)
-
-    elif message.text.lower() == 'отзывы преподователей?':
-        bot.send_message(message.chat.id, 'преподователь: ' + otzivyh + ' отзыв: ' + otziv)
-
-    elif message.text.lower() == '<назад>':
-        bot.send_message(message.chat.id, '<==>', reply_markup=keyboard3)
-
-    elif message.text.lower() == '<возврат в начало>':
-        bot.send_message(message.chat.id, '<=', reply_markup=keyboard1)
-
-    elif message.text.lower() == '<возврат в начало>':
-        bot.send_message(message.chat.id, '<=', reply_markup=keyboard1)
-
-    elif message.text.lower() == 'cтоимость занятий' or message.text.lower() == 'cтоимость занятий?' \
-        or message.text.lower() == 'стоимость обучения?' or message.text.lower() == 'стоимость обучения':
-        bot.send_message(message.chat.id, 'Стоимость обучения: ...')
+# флаг языка
+lang = 'en'
+# флаг что мы ожидаем авторизацию
+awaitAuth = 0
+# login user
+uLogin = ''
+# password user
+uPsswd = ''
 
 
-    elif message.text.lower() == 'когда каникулы' or message.text.lower() == 'когда каникулы?' \
-        or message.text.lower() == 'когда конец обучения?' or message.text.lower() == 'когда конец обучения':
-        bot.send_message(message.chat.id, 'Каникулы будут с ' + a + ' по ' + b)
+######
+# все функции   начинающиеся с btn_ - выводят кнопки
+#               начинающиеся c save_ - сохраняют введенные пользователем данные
 
+# ----------------------------------------------------------Авторизация на русском----------------------------------------------------------
+@bot.message_handler(func=lambda message: message.text.lower() == 'русский')
+def btn_login_ru(message):
+    global awaitAuth, lang
+    awaitAuth = 1
+    lang = 'ru'
+    k = telebot.types.ReplyKeyboardMarkup()
+    k.row('Забыл логин')
+    k.row('<Возврат в начало>')
+    bot.send_message(message.chat.id, 'Введите ваш логин:', reply_markup=k)
+
+
+# ----------------------------------------------------------Авторизация на английском----------------------------------------------------------
+@bot.message_handler(func=lambda message: message.text.lower() == 'english')
+def btn_login_en(message):
+    global awaitAuth, lang
+    awaitAuth = 1
+    lang = 'en'
+    k = telebot.types.ReplyKeyboardMarkup()
+    k.row('Forget login')
+    k.row('<Return to start>')
+    bot.send_message(message.chat.id, 'Enter login:', reply_markup=k)
+
+
+# ----------------------------------------------------------Сохранение логина----------------------------------------------------------
+@bot.message_handler(func=lambda message: message.content_type == 'text' and awaitAuth == 1)
+def save_user_login(message):
+    global uLogin, lang, awaitAuth
+    awaitAuth = 2
+    uLogin = message.text
+    k = telebot.types.ReplyKeyboardMarkup()
+    k.row('Забыл пароль')
+    k.row('<Возврат в начало>')
+    bot.send_message(message.chat.id, dictMsg[lang]['Введите пароль'])
+
+
+# ----------------------------------------------------------Сохранение пароля----------------------------------------------------------
+@bot.message_handler(func=lambda message: message.content_type == 'text' and awaitAuth == 2)
+def save_user_passwd(message):
+    global uLogin, uPsswd, lang, awaitAuth
+    awaitAuth = 0
+    uPsswd = message.text
+    if uLogin == 'admin1' and uPsswd == 'admin2':
+        btn_welcome(message)
     else:
-        bot.send_message(message.from_user.id, "Я вас не понимаю")
+        bot.send_message(message.chat.id, 'Oops!')
+        if lang == 'ru':
+            btn_login_ru(message)
+        else:
+            btn_login_en(message)
+
+# ----------------------------------------------------------Ученик / Родитель----------------------------------------------------------
+def btn_welcome(message):
+    k = telebot.types.ReplyKeyboardMarkup()
+    k.row(dictMsg[lang]['Ученик'])
+    k.row(dictMsg[lang]['Родитель'])
+    k.row(dictMsg[lang]['<Возврат в начало>'])
+    bot.send_message(message.chat.id, dictMsg[lang]['Welcome'], reply_markup=k)
 
 
-@bot.message_handler(content_types=['sticker'])
-def sticker_id(message):
-    print(message)
+# ----------------------------------------------------------Панель ученика----------------------------------------------------------
+@bot.message_handler(func=lambda message: message.text.lower() == 'ученик' or message.text.lower() == 'student')
+def student_panel(message):
+    k = telebot.types.ReplyKeyboardMarkup()
+    k.row(dictMsg[lang]['Средний балл'], dictMsg[lang]['Посещаемость'], dictMsg[lang]['Мероприятия'])
+    k.row(dictMsg[lang]['Конец обучения'], dictMsg[lang]['Отзывы преподавателей'])
+    k.row(dictMsg[lang]['<Назад>'])
+    if lang == 'ru':
+        bot.send_message(message.chat.id, 'Что вы хотите узнать?', reply_markup=k)
+    else:
+        bot.send_message(message.chat.id, 'What do you want to know?', reply_markup=k)
+
+
+# ----------------------------------------------------------Панель родителя----------------------------------------------------------
+@bot.message_handler(func=lambda message: message.text.lower() == 'родитель' or message.text.lower() == 'parent')
+def parent_panel(message):
+    k = telebot.types.ReplyKeyboardMarkup()
+    k = telebot.types.ReplyKeyboardMarkup()
+    k.row(dictMsg[lang]['Средний балл'], dictMsg[lang]['Посещаемость'], dictMsg[lang]['Стоимость обучения'])
+    k.row(dictMsg[lang]['Конец обучения'], dictMsg[lang]['Отзывы преподавателей'], dictMsg[lang]['Мероприятия'])
+    k.row(dictMsg[lang]['<Назад>'])
+    if lang == 'ru':
+        bot.send_message(message.chat.id, 'Что вы хотите узнать?', reply_markup=k)
+    else:
+        bot.send_message(message.chat.id, 'What do you want to know?', reply_markup=k)
+
+
+# ----------------------------------------------------------Средний балл----------------------------------------------------------
+@bot.message_handler(func=lambda message: message.text.lower() == 'средний балл' or message.text.lower() == 'average score')
+def average_score(message):
+    if lang == 'ru':
+        bot.send_message(message.chat.id, 'Ваш средний балл: 11')
+    else:
+        bot.send_message(message.chat.id, 'Your average score: 11')
+
+
+# ----------------------------------------------------------Посещаемость----------------------------------------------------------
+@bot.message_handler(func=lambda message: message.text.lower() == 'посещаемость' or message.text.lower() == 'attendance')
+def attendance(message):
+    if lang == 'ru':
+        bot.send_message(message.chat.id, 'Ваша посещаемость: 100%')
+    else:
+        bot.send_message(message.chat.id, 'Your attendance: 100%')
+
+
+# ----------------------------------------------------------Мероприятия----------------------------------------------------------
+@bot.message_handler(func=lambda message: message.text.lower() == 'мероприятия' or message.text.lower() == 'events')
+def events(message):
+    if lang == 'ru':
+        bot.send_message(message.chat.id, 'Будущие мероприятия: Летний лагер ШАГ')
+    else:
+        bot.send_message(message.chat.id, 'Future events: Summer ISTEP camp')
+
+
+# ----------------------------------------------------------Стоимость обучения----------------------------------------------------------
+@bot.message_handler(func=lambda message: message.text.lower() == 'стоимость обучения' or message.text.lower() == 'cost training')
+def cost_training(message):
+    if lang == 'ru':
+        bot.send_message(message.chat.id, 'Стоимость: 15 000 рублей')
+    else:
+        bot.send_message(message.chat.id, 'Cost: 200 dollars')
+
+
+# ----------------------------------------------------------Конец обучения----------------------------------------------------------
+@bot.message_handler(func=lambda message: message.text.lower() == 'конец обучения' or message.text.lower() == 'the end of the training')
+def end_training(message):
+    if lang == 'ru':
+        bot.send_message(message.chat.id, 'Обучение закончится: 31 Мая')
+    else:
+        bot.send_message(message.chat.id, 'Training will end: 31 May')
+
+
+# ----------------------------------------------------------Отзывы преподавателей----------------------------------------------------------
+@bot.message_handler(func=lambda message: message.text.lower() == 'отзывы преподавателей' or message.text.lower() == 'teachers feedback')
+def teachers_feedback(message):
+    if lang == 'ru':
+        bot.send_message(message.chat.id, 'Леонид: Способный малый, вообще клёвый парень')
+    else:
+        bot.send_message(message.chat.id, 'Jhon: Good boy, very cool on pairs')
 
 
 bot.polling()
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Deadl\PycharmProjects\35451\venv"
+set "VIRTUAL_ENV=C:\Users\Deadl\PycharmProjects\TestingShagBot\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
